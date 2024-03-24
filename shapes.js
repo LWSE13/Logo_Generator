be4db833d1b200164cc2ef3451eb2389f1d3be77
LWSE13 <152429230+LWSE13@users.noreply.github.com>
2024-03-24 22:01:03 +0000

    defined shape subclasses and overrided shape specific methods

--- shapes.js
+++ shapes.js
@@ -1,12 +1,30 @@
 class Shape {
     constructor(colour, size) {
-        this.colour = 'colour',
-        this.size = 'size'
+        this.colour = 'colour';
+        this.size = 'size';
     }
 }
 
 class Circle extends Shape {
+
+    // shape generation takes the desired shape size and divides by two to create a new variable 
+    // called radius that is used to define the circle's x, y and r attributes
+    //circle is encapsulated within < /> as it is a HTML element
     shapeGeneration() {
-        return ``
+        const radius = this.size / 2;
+        return `<circle cx=${radius} cy=${radius} r=${radius} fill=${this.colour} />`;
+    }
+}
+
+class Square extends Shape {
+    shapeGeneration() {
+        return `<rect width=${this.size} height=${this.size} fill=${this.colour}/>`;
+    }
+}
+
+class Triangle extends Shape {
+    shapeGeneration() {
+        const sides  =this.size * Math.sqrt(3) / 2;
+        return `<polygon points='0,${height} ${this.size / 2},0 ${this.size},${height}" fill='${this.color}" />`;
     }
 }
