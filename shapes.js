0a1e85fb14026dc8f919e1e168063f884768b599
LWSE13 <152429230+LWSE13@users.noreply.github.com>
2024-03-24 21:29:45 +0000

    added shapes.js created shape superclass and begun work on subclasses

---
+++ shapes.js
@@ -0,0 +1,12 @@
+class Shape {
+    constructor(colour, size) {
+        this.colour = 'colour',
+        this.size = 'size'
+    }
+}
+
+class Circle extends Shape {
+    shapeGeneration() {
+        return ``
+    }
+}
