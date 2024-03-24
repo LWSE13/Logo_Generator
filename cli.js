acb2f9581896e4aac178783f61c69bf519eb7a32
LWSE13 <152429230+LWSE13@users.noreply.github.com>
2024-03-24 00:13:03 +0000

    write basic code for index and begun work on cli classes in cli.js

---
+++ cli.js
@@ -0,0 +1,24 @@
+const inquirer = require('inquirer');
+const { writefile }  = require('fs/promises');
+const { join } = require('path');
+const { createDocument } = require('./document');
+
+class CLI {
+    constructor() {
+        this.title = '';
+        this.tasks = [];
+
+    }
+    run() {
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is your name?',
+            },
+        ])
+        .then(({ name }) => {
+            
+        })
+    }
+}
