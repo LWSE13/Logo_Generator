20d217d76a947df7a85f12da1f3a55fa4a2b3f42
LWSE13 <leewheeler888@gmail.com>
2024-03-26 01:20:47 +0000

    updated classes and tests to fit SVG acceptance criteria dimensions

--- shapes.js
+++ shapes.js
@@ -11,21 +11,21 @@ class Circle extends Shape {
     // called radius that is used to define the circle's x, y and r attributes
     //circle is encapsulated within < /> as it is a HTML element
     shapeGeneration() {
-        const radius = this.size / 2;
-        return `<circle cx=${radius} cy=${radius} r=${radius} fill=${this.colour}/>`;
+        
+        return `<circle cx="150" cy="100" r="80" fill=${this.colour}/>`;
     }
 }
 
 class Square extends Shape {
     shapeGeneration() {
-        return `<rect width=${this.size} height=${this.size} fill=${this.colour}/>`;
+        return `<rect x="90" y="40" width="120" height="120" fill=${this.colour}/>`;
     }
 }
 
 class Triangle extends Shape {
     shapeGeneration() {
         const sideHeight  =this.size * Math.sqrt(3) / 2;
-        return `<polygon points='0,${sideHeight} ${this.size / 2},0 ${this.size},${sideHeight}' fill='${this.colour}'/>`;
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.colour}"/>`;
     }
 }
 
--- shapes.test.js
+++ shapes.test.js
@@ -3,21 +3,21 @@ const { Circle, Square, Triangle } = require('./shapes');
 describe('shapes', () => {
     describe('Circle', () => {
         it ('should correctly return a circle SVG string with the desired deimensions', () => {
-            const circleTest = new Circle('red', 50);
-            expect(circleTest.shapeGeneration()).toBe('<circle cx=25 cy=25 r=25 fill=red/>')
+            const circleTest = new Circle('red');
+            expect(circleTest.shapeGeneration()).toBe('<circle cx="150" cy="100" r="80" fill=red/>')
         })
     })
     describe('Square', () => {
         it ('should correctly return a square SVG string with the desired deimensions', () => {
             const squareTest = new Square ('blue', 50);
-            expect(squareTest.shapeGeneration()).toBe('<rect width=50 height=50 fill=blue/>')
+            expect(squareTest.shapeGeneration()).toBe('<rect x="90" y="40" width="120" height="120" fill=blue/>')
         })
     })
     describe('Triangle', () => {
         it ('should correctly return a triangle SVG string with the desired deimensions', () => {
             const triangleTest = new Triangle ('green', 50);
             const expectedHeight = 50 * Math.sqrt(3) / 2;
-            expect(triangleTest.shapeGeneration()).toBe(`<polygon points='0,${expectedHeight} 25,0 50,${expectedHeight}' fill='green'/>`)
+            expect(triangleTest.shapeGeneration()).toBe(`<polygon points="150, 18 244, 182 56, 182" fill="green"/>`)
         })
     })
 })
